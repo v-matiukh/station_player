c489f4ae66d69f83e4304fb33e951c91e8b6bb3b
Volodymyr Matiukh <vova.matiukh@gmail.com>
2024-08-26 20:48:34 +0300

    Initial Commit

---
+++ station-player/station_playerApp.swift
@@ -0,0 +1,17 @@
+//
+//  station_playerApp.swift
+//  station-player
+//
+//  Created by Volodymyr Matiukh on 26.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct station_playerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
